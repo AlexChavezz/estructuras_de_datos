6f3d9d0fac190525cd2d12fcb4915ccd3fbd32e2
Mac27 <mac27@Mac27s-iMac.local>
2022-11-11 13:55:06 -0600

    changes

--- lista_ligada.cpp
+++ lista_ligada.cpp
@@ -23,15 +23,46 @@ class cLista
         n=0;
         siguiente=NULL;
     }
-    void crearLista(int n);
+    void crearLista();
     void mostrarLista(cLista* lista);
 };
 
-void cLista::crearLista(int numero)
+void cLista::crearLista()
 {
-    cLista* lista = new cLista();
-    lista->n = numero;
-    cout<< numero<<" asingnado"<<endl;
+    int size;
+    cout<<"Cuantos noodos requieres en la lista"<<endl;
+    cin>>size;
+    for(int i=0;i<size;i++)
+    {
+        cLista* nuevo=new cLista();
+        cout<<"Ingresa el dato del nodo "<<i+1<<endl;
+        cin>>nuevo->n;
+        if(siguiente==NULL)
+        {
+            siguiente=nuevo;
+        }
+        else
+        {
+            cLista* aux=siguiente;
+            while(aux->siguiente!=NULL)
+            {
+                aux=aux->siguiente;
+            }
+            aux->siguiente=nuevo;
+        }
+
+    }
+    // cLista* lista = new cLista();
+    // lista->n = numero;
+    // cout<< numero<<" asingnado"<<endl;
+    for(int i=0; i<size; i++)
+    {
+        cLista
+        // lista->siguiente = new cLista();
+        // lista = lista->siguiente;
+        // lista->n = i;
+        // cout<< i<<" asingnado"<<endl;
+    }
     // cLista* lista = new cLista;
     // cLista* aux = lista;
     // int n;
